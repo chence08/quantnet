1ba6c817841aaf14180c331b1674016106a333a1
YiJia Chen <chence12@gmail.com>
2022-01-25 06:18:00 +0800

    test cron

---
+++ sandbox/quiz4.cpp
@@ -0,0 +1,19 @@
+/* Author: YiJia Chen */
+
+#include <iostream>
+
+int x=10;
+
+namespace
+{
+  int x=20;
+}
+
+int main(int x, char* y[])
+{
+  {
+    int x=30;
+    std::cout<<::x<<std::endl;
+  }
+  return 0;
+}
